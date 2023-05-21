104ef41ca002d8609d88a87c504ac7b2329fd122
Alexander Kuzmenko <a.kuzmenko@outlook.de>
2023-05-21 12:46:22 +0200

    first Commit

---
+++ src/main/resources/gfn/model/entity/Mitglied_.java
@@ -0,0 +1,16 @@
+package gfn.model.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2023-05-21T11:43:34.994+0200")
+@StaticMetamodel(Mitglied.class)
+public class Mitglied_ {
+	public static volatile SingularAttribute<Mitglied, Integer> mitgliedId;
+	public static volatile SingularAttribute<Mitglied, String> mitgliedName;
+	public static volatile SingularAttribute<Mitglied, String> mitgliedVorname;
+	public static volatile SingularAttribute<Mitglied, String> email;
+	public static volatile SingularAttribute<Mitglied, String> mitgliedTelefonnummer;
+	public static volatile SingularAttribute<Mitglied, String> passwort;
+}
